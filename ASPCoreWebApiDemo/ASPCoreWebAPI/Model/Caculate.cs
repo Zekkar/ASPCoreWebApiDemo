eae040dc4e6e8fc77a668b6773a396db83b3e62b
ds.hsu <Ds198634>
2019-02-11 15:23:13 +0800

    增加方法

---
+++ ASPCoreWebApiDemo/ASPCoreWebAPI/Model/Caculate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreWebAPI.Model
+{
+    public class Caculate
+    {
+        public int addNum(int a , int b)
+        {
+            try
+            {
+                int sum = a +b;
+
+                return sum;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
