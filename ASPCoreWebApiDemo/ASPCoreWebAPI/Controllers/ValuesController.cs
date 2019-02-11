79a991dfdcb8b723fe0e127ce939e8ead484df38
ds.hsu <Ds198634>
2019-02-11 14:23:10 +0800

    增加專案

---
+++ ASPCoreWebApiDemo/ASPCoreWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPCoreWebAPI.DTO;
+using Newtonsoft.Json;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            List<DemoDTO> reList = new List<DemoDTO>()
+            {
+                new DemoDTO {number = "1" },
+                new DemoDTO {number = "121" }
+            };
+
+            string jsondata = JsonConvert.SerializeObject(reList);
+
+
+            return jsondata;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ASPCoreWebApiDemo/ASPCoreWebAPI/DTO/DemoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreWebAPI.DTO
+{
+    public class DemoDTO
+    {
+        public string number { get; set; }
+    }
+}
