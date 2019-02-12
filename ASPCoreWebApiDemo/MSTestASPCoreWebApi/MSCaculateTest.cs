454f18d761df867d15b0ed842d245fec0510f89e
ds.hsu <Ds198634>
2019-02-12 15:43:22 +0800

    加入Nunit套件

---
+++ ASPCoreWebApiDemo/MSTestASPCoreWebApi/MSCaculateTest.cs
@@ -0,0 +1,70 @@
+﻿using ASPCoreWebAPI.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace MSTestASPCoreWebApi
+{
+    [TestClass]
+    public class MSCaculateTest
+    {
+        private MockRepository mockRepository;
+
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            this.mockRepository.VerifyAll();
+        }
+
+        private Caculate CreateCaculate()
+        {
+            return new Caculate();
+        }
+
+        [TestMethod]
+        public void AddNum_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = this.CreateCaculate();
+            int a = 1;
+            int b = 2;
+            int expected = 3;
+            int actual;
+
+            // Act
+            actual = unitUnderTest.AddNum(a, b);
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void LessNum_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = this.CreateCaculate();
+            int a = 1;
+            int b = 2;
+            int expected = 1;
+            int actual;
+
+            // Act
+            actual = unitUnderTest.LessNum(b, a);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
