275cbbbc7a27eb3fcacd230eb23d1da7dad6f7b0
ds.hsu <Ds198634>
2019-02-13 17:23:27 +0800

    改正UnitTest

--- ASPCoreWebApiDemo/MSTestASPCoreWebApi/Model/CaculateTests.cs
+++ ASPCoreWebApiDemo/MSTestASPCoreWebApi/Model/CaculateTests.cs
@@ -57,7 +57,7 @@ namespace MSTestASPCoreWebApi.Model
             var unitUnderTest = this.CreateCaculate();
             int a = 1;
             int b = 2;
-            int expected = ;
+            int expected = 1;
             int actual;
 
             // Act
